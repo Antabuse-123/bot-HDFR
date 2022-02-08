5cfd6bb5472dc21207032701543f37d6da3a6695
Antabuse-does-something <Antabuse-does-something@users.noreply.github.com>
2022-02-07 23:20:35 -0500

    Bugfix : DM interaction

--- src/ressouces/challenges.js
+++
@@ -1,8 +0,0 @@
-class Challenge {
-    constructor(title, description, score, id_author){
-        this.title = title;
-        this.description = description;
-        this.score = score;
-        this.id_author = id_author;
-    }
-}
